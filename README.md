70ea68b8fbd76609f7065f012d03fe679fc54ef1
barkure <43804451+barkure@users.noreply.github.com>
2024-05-08 18:02:48 +0800

    feat: add image upload to Cloudflare R2 and push to QQ by Qmsg

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## 3X-UI-Traffic
-Statistics of each client’s traffic through 3X-UI’s API, Send statistics image to emails.
+- Statistics of each client’s traffic through 3X-UI’s API
+- Upload statistics image to Cloudflare R2
+- Push to QQ by Qmsg
+- Send to Emails
--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
+import boto3
 
 # Config
 # 3X-UI Config
@@ -13,14 +14,28 @@ base_url = ''
 username = ''
 password = ''
 # Email Config
+send_email = False
 smtp_server = 'smtp.office365.com'
 smtp_port = 587
 email_address = ''
 email_password = ''
-to_address = ['abc@xxx.com', 'wsgwy@jack.com']
+to_address = []
+# S3 Config
+upload_to_r2 = True
+endpoint_url = ''
+aws_access_key_id = ''
+aws_secret_access_key = ''
+bucket_name = ''
+custom_domain = ''
+# Qmsg Config
+push_to_qmsg = True
+qmsg_key = ''
+group_number = ''
+
 
 # Disable SSL warnings
 urllib3.disable_warnings()
+
 
 # Login and get cookie
 login_url = f'{base_url}/login?username={username}&password={password}'
@@ -28,12 +43,14 @@ login_response = requests.post(login_url, verify=False)
 print(login_response.text)
 cookie = login_response.cookies.get_dict()
 
+
 # Get list of inbounds
 url = f'{base_url}/panel/api/inbounds/list'
 headers = {
     'Cookie': 'session=' + cookie['session']
 }
 response = requests.get(url, verify=False, headers=headers)
+
 
 # Parse the response, get traffics data and calculate the amount
 unit_cost = 0.65 # CNY/GiB
@@ -69,30 +86,58 @@ image = Image.new('RGB', (1080, 600), color = (0, 0, 0))
 now = datetime.now()
 time_str = now.strftime("%m/%d/%Y %H:%M:%S")
 text = f'{table_str}\n\nStatistics at {time_str}.'
-
+# Create draw object
 d = ImageDraw.Draw(image)
 # loading equal width font
 font = ImageFont.truetype("fonts/Ubuntu Mono derivative Powerline.ttf", 30)
+# draw text on image
 d.text((20,20), text, fill=(255,255,255), font=font)
-image.save('text.png')
+# get timestamp
+timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+# save image
+image.save(f'images/{timestamp}.png')
+
+
+# Upload the image to R2
+if upload_to_r2:
+    s3 = boto3.resource('s3',
+        endpoint_url = endpoint_url,
+        aws_access_key_id = aws_access_key_id,
+        aws_secret_access_key = aws_secret_access_key
+    )
+
+    bucket = s3.Bucket('assets')
+    bucket.upload_file(f'images/{timestamp}.png', f'traffic/{timestamp}.png')
+    image_url = f'{custom_domain}/traffic/{timestamp}.png'
+    print(f'Image uploaded to {image_url}')
+
+
+# Push to QQ by Qmsg
+if push_to_qmsg:
+    qmsg_url = f'https://qmsg.zendee.cn/group/{qmsg_key}'
+    params = {
+        'msg': f'daily report@image={image_url}@',
+        'qq': group_number,
+    }
+    requests.post(qmsg_url, params=params)
 
 
 # Send the image to Email
-msg = MIMEMultipart()
-msg['Subject'] = 'Traffic Statistics'
-msg['From'] = email_address
-msg['To'] = ', '.join(to_address)
+if send_email:
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Traffic Statistics'
+    msg['From'] = email_address
+    msg['To'] = ', '.join(to_address)
 
-# Attach the image
-with open('text.png', 'rb') as f:
-    image = MIMEImage(f.read())
-    msg.attach(image)
+    # Attach the image
+    with open('text.png', 'rb') as f:
+        image = MIMEImage(f.read())
+        msg.attach(image)
 
-# Send the email
-server = smtplib.SMTP(smtp_server, smtp_port)
-server.starttls()
-server.login(email_address, email_password)
-server.send_message(msg)
-server.quit()
-print('Email sent successfully')
-
+    # Send the email
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(email_address, email_password)
+    server.send_message(msg)
+    server.quit()
+    print('Email sent successfully')
